b815660c56700330d235fb5e7f261673a126be9b
Mimo604 <selim.abdelwahab@mail.utoronto.ca>
2024-10-30 21:37:18 -0400

    Dataset+Helpers
    Everyone should run: pip install -r requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # APS360_Project
+## Everyone should run pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-# hi pookies
+from src.helper import *
+
+train_loader, val_loader, test_loader, target_classes = get_data_loader(256)
