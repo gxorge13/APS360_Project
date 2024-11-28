09fdb2c4228f8b5b365375d60112444cba2710e0
gxorge13 <georgewagdy123@gmail.com>
2024-11-27 20:14:26 -0500

    Changed confusion.py so that confusion function also returns f1 scores, precision and accuracy

--- src/confusion.py
+++ src/confusion.py
@@ -4,9 +4,10 @@ from classifier import *
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score, f1_score, precision_score, recall_score
 
-def compute_confusion_matrix(net, dataloader, classes):
+def eval_net(net, dataloader, classes):
+   #eval_net
    net.eval() 
    all_preds = []
    all_labels = []
@@ -25,24 +26,33 @@ def compute_confusion_matrix(net, dataloader, classes):
    
    all_preds = np.concatenate(all_preds)
    all_labels = np.concatenate(all_labels)
-   
+
+   accuracy = accuracy_score(all_labels, all_preds)
+   f1 = f1_score(all_labels, all_preds, average=None)  
    cm = confusion_matrix(all_labels, all_preds, labels=range(len(classes)))
-   return cm
+   precision = precision_score(all_labels, all_preds, average=None) 
+   recall = recall_score(all_labels, all_preds, average=None)  
+   return cm, f1, accuracy, precision, recall
 
 
 def plot_confusion_matrix(cm, classes, title="Confusion Matrix"):
+   plt.figure(figsize=(10, 10))  # Adjust the width and height as needed
    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=classes)
-   disp.plot(cmap=plt.cm.Blues, xticks_rotation='vertical')
+   disp.plot(cmap=plt.cm.Blues, xticks_rotation='vertical', ax=plt.gca())
    plt.title(title)
    plt.show()
 
 
 if __name__ == "__main__":
    dir = "./models/vgg/features"
-   testDS = torchvision.datasets.DatasetFolder(f"{dir}/test",
+   # testDS = torchvision.datasets.DatasetFolder(f"{dir}/test",
+   #                                              loader=torch.load,
+   #                                              extensions=".tensor")
+   
+   valDS = torchvision.datasets.DatasetFolder(f"{dir}/val",
                                                 loader=torch.load,
                                                 extensions=".tensor")
-   test_loader = torch.utils.data.DataLoader(testDS, 256, shuffle=True)
+   val_loader = torch.utils.data.DataLoader(valDS, 256, shuffle=True)
    *_, classes = get_data_loader(1) 
 
    net = Classifier("model3")
@@ -50,5 +60,7 @@ if __name__ == "__main__":
    net.load_state_dict(torch.load(model_path))
    net.to("cuda" if torch.cuda.is_available() else "cpu")
 
-   cm = compute_confusion_matrix(net, test_loader, classes)
+   cm, f1, accuracy, precision, recall = eval_net(net, val_loader, classes)
    plot_confusion_matrix(cm, classes, title="Confusion Matrix")
+   print(f"f1 score: {f1}, accuracy: {accuracy}, precision: {precision}, recall: {recall}")
+#f1 score: [0.85522788 0.87128713 0.96818811], accuracy: 0.8979779411764706, precision: [0.85066667 0.87749288 0.96685083], recall: [0.85983827 0.86516854 0.96952909]
--- src/train.py
+++ src/train.py
@@ -1,7 +1,10 @@
 from classifier import *
 
-myClassifier = Classifier("model3")
+# myClassifier = Classifier("model3")
 
-train_net_with_features(myClassifier, batch_size = 256, learning_rate=0.001, num_epochs = 20)
+# train_net_with_features(myClassifier, batch_size = 256, learning_rate=0.001, num_epochs = 20)
 
-plot_training_curve(f"./models/vgg_transfer_classifier/{myClassifier.name}_BS={256}_LR={0.001}_EP={19}")
+# plot_training_curve(f"./models/vgg_transfer_classifier/{myClassifier.name}_BS={256}_LR={0.001}_EP={19}")
+plot_training_curve(f"./models/vgg_transfer_classifier/model3_BS={256}_LR={0.001}_EP={19}")
+
+# C:\Users\georg\OneDrive\Desktop\SideProjects\APS360_Project\models\vgg_transfer_classifier\model3_BS=256_LR=0.001_EP=19
